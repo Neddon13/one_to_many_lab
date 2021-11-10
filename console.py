e5835f7ed390e1789079c60a75ecfe9e3e5bbbca
Neddon13 <91260623+Neddon13@users.noreply.github.com>
2021-11-10 16:34:37 +0000

    mvp done

--- console.py
+++ console.py
@@ -4,6 +4,7 @@ from models.author import Author
 import repositories.book_repository as book_repository
 import repositories.author_repository as author_repository 
 
+
 author_1 = Author("J.K.Rowling")
 author_repository.save(author_1)
 author_2 = Author("Stephen King")
@@ -22,5 +23,4 @@ book_repository.save(book_4)
 for book in book_repository.select_all():
     print(book.__dict__)
 
-
-
+book_repository.delete(1)
